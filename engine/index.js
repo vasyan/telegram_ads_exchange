5c51c22f1454cde40e04542ced82c14e23e08bdf
Vasyan <vasilyator@gmail.com>
2018-02-13 15:27:43 +0300

    Вынесение токена бота в переменные окружения

--- engine/index.js
+++ engine/index.js
@@ -1,6 +1,5 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api')
 
-const token = '480803172:AAEVQblEt06E-mZCPjWl6pptRsMzfcJzRb8';
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true })
 
 module.exports = bot
