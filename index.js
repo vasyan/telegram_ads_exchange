e6faf667f87844eebc1a9e3bf5d27bdc830f025a
Vasyan <vasilyator@gmail.com>
2018-02-26 14:27:22 +0700

    Выносит подключение к базе в отдельный модуль

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const bot = require('./engine')
-const mongoose = require('mongoose')
-const Views = require('./views')
 // const Menus = require('./menus')
-const SchemaUser = require('./schema/user')
+require('./views')
+require('./db')
 
-mongoose.connect('mongodb://' + process.env.MONGO_URL)
-
-const db = mongoose.connection
-
-db.on('error', console.error.bind(console, 'connection error:'))
-
-var ModelUser = mongoose.model('User', SchemaUser)
+// var ModelUser = mongoose.model('User', SchemaUser)
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
@@ -25,37 +18,31 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 bot.onText(/\/register/, (msg, match) => {})
 
-bot.onText(/\/whoami/, (msg, match) => {
-  const chatId = msg.chat.id
-  const username = msg.chat.username
-
-  ModelUser.find({ userId: chatId }, function(err, users) {
-    if (users.length === 0) {
-      const user = new ModelUser({
-        userId: chatId,
-        username: username,
-      })
-
-      user.save(function(err, user) {
-        if (err) {
-          return console.error('Save model error', err)
-        }
-
-        bot.sendMessage(chatId, 'Entity created')
-      })
-    } else {
-      bot.sendMessage(chatId, 'Hello, ' + users[0].username)
-    }
-  })
-
-  bot.sendMessage(chatId, 'nope').catch(function(err) {
-    console.log('Err', err)
-  })
+bot.onText(/\/whoami/, msg => {
+  // const chatId = msg.chat.id
+  // const username = msg.chat.username
+  // ModelUser.find({ userId: chatId }, function(err, users) {
+  //   if (users.length === 0) {
+  //     const user = new ModelUser({
+  //       userId: chatId,
+  //       username: username,
+  //     })
+  //     user.save(function(err, user) {
+  //       if (err) {
+  //         return console.error('Save model error', err)
+  //       }
+  //       bot.sendMessage(chatId, 'Entity created')
+  //     })
+  //   } else {
+  //     bot.sendMessage(chatId, 'Hello, ' + users[0].username)
+  //   }
+  // })
+  // bot.sendMessage(chatId, 'nope').catch(function(err) {
+  //   console.log('Err', err)
+  // })
 })
 
 bot.on('callback_query', payload => {})
-
-let inited = false
 
 bot.on('message', payload => {})
 
